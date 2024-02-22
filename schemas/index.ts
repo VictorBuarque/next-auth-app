7b39e2fbffc7d6c84ea9d0217e1a4624b2f90f0a
Victor Buarque <vcgabriel38@gmail.com>
2024-02-22 19:16:38 -0300

    feat: create register schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -8,3 +8,15 @@ export const LoginSchema = z.object({
         message: "Please insert your password"
     }), // on login its recommended dont use .min()
 });
+export const RegisterSchema = z.object({
+    email: z.string().email({
+        message: "Please insert your e-mail"
+    }),
+    password: z.string().min(6,{
+        message: "Minimum 6 characters password required"
+    }), // on login its recommended dont use .min()
+    name: z.string().min(3,{
+        message: "Name is required"
+    }), // on login its recommended dont use .min()
+});
+
