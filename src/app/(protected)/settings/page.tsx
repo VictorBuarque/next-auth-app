08cca31e8f3d5f1db404c5ceb0ce3f6422370d49
Victor Buarque <vcgabriel38@gmail.com>
2024-02-28 18:39:13 -0300

    feat: create settings page

---
+++ src/app/(protected)/settings/page.tsx
@@ -0,0 +1,8 @@
+import { auth } from "@/lib/auth";
+
+const SettingsPage = async () => {
+  const session = await auth();
+  return <div>{JSON.stringify(session)}</div>;
+};
+
+export default SettingsPage;
