abdf63a16b4b169eb6603d9b271bf88567f26fed
Victor Buarque <vcgabriel38@gmail.com>
2024-02-29 22:01:56 -0300

    feat: create credentials providers

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -1,7 +1,28 @@
-import GitHub from "next-auth/providers/github"
+import bcrypt from "bcryptjs";
+import { NextAuthConfig } from "next-auth";
+import Credentials from 'next-auth/providers/credentials';
+import { LoginSchema } from "../../schemas";
+import { getUserByEmail } from "../../data/user";
 
-import type { NextAuthConfig } from "next-auth"
+const nextAuthConfig: NextAuthConfig = {
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validateFields = LoginSchema.safeParse(credentials);
+        if (validateFields.success) {
+          const { email, password } = validateFields.data;
+          console.log(validateFields.data);
 
-export default {
-  providers: [GitHub],
-} satisfies NextAuthConfig
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null; //This compare who sign in with google or github
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) return user;
+        }
+        return null;
+      }
+    })
+  ],
+};
+
+export default nextAuthConfig;
