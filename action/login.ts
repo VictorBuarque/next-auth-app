cb0621fa8ca8aec45a59d92c3b7f122be98c0b57
Victor Buarque <vcgabriel38@gmail.com>
2024-02-29 22:01:00 -0300

    feat: create login credentials

--- action/login.ts
+++ action/login.ts
@@ -1,14 +1,35 @@
 "use server";
 import * as z from 'zod';
-
+import { signIn } from '@/lib/auth';
 import { LoginSchema } from "../schemas";
+import { DEFAULT_LOGIN_REDIRECT, publicRoutes } from '@/lib/routes';
+import { AuthError } from 'next-auth';
 
-export const login =  async (values: z.infer<typeof LoginSchema>) => {
+export const login = async (values: z.infer<typeof LoginSchema>) => {
     const validateFields = LoginSchema.safeParse(values);
 
-    if (!validateFields.success){
-        return {error: "Invalid fields!"};
+    if (!validateFields.success) {
+        return { error: "Invalid fields!" };
     }
 
-    return { success: "Email Sent"}
+    const { email, password } = validateFields.data;
+
+    try {
+        await signIn("credentials", {
+            email,
+            password,
+            redirectTo: DEFAULT_LOGIN_REDIRECT //THIS WILL BE THE REDIRECT WITH LOGIN
+        })
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case "CredentialsSignin":
+                    return { error: "Invalid credentials"};
+                default:
+                    return { error: "Unexpected error, something went wrong!" }
+            }
+        }
+        throw error;
+    }
+
 };
